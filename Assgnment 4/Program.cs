18469d4f6aa4df6005a56318b22e6a4b61d0d861
visualstudio <visualstudio@122-PC35>
2019-03-27 10:22:12 -0400

    Initial commit

--- Assgnment 4/Program.cs
+++ Assgnment 4/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace Assgnment_4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Download file");
+            Download();
+            Console.ReadLine();
+
+        }
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete ");
+            });
         }
     }
 }
